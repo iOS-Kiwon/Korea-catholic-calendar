e2946e2c32d8e1cf166ec38b51f89909375064e2
기원 이 <kiwon.yee@coinone.com>
2026-07-23 11:58:33 +0900

    설정 화면 백업 섹션 숨김(unsupported) 테스트 추가

--- test/settings_page_test.dart
+++ test/settings_page_test.dart
@@ -14,14 +14,24 @@ class _AvailStore extends PersonalCloudBackupStore {
       CloudBackupAvailability.available;
 }
 
-Future<void> _pump(WidgetTester tester, {required bool updateAvailable}) async {
+class _UnsupportedStore extends PersonalCloudBackupStore {
+  @override
+  Future<CloudBackupAvailability> checkAvailability() async =>
+      CloudBackupAvailability.unsupported;
+}
+
+Future<void> _pump(
+  WidgetTester tester, {
+  required bool updateAvailable,
+  PersonalCloudBackupStore? store,
+}) async {
   SharedPreferences.setMockInitialValues({});
   final prefs = await SharedPreferences.getInstance();
   await tester.pumpWidget(
     ProviderScope(
       overrides: [
         sharedPreferencesProvider.overrideWith((ref) async => prefs),
-        personalCloudBackupStoreProvider.overrideWithValue(_AvailStore()),
+        personalCloudBackupStoreProvider.overrideWithValue(store ?? _AvailStore()),
         packageInfoProvider.overrideWith(
           (ref) async => PackageInfo(
             appName: '가톨릭 달력',
@@ -54,5 +64,16 @@ void main() {
   testWidgets('업데이트가 있으면 안내 문구를 표시한다', (tester) async {
     await _pump(tester, updateAvailable: true);
     expect(find.textContaining('새로운 기능을 만나보세요'), findsOneWidget);
+  });
+
+  testWidgets('백업 미지원(unsupported)이면 백업 섹션을 숨긴다', (tester) async {
+    await _pump(
+      tester,
+      updateAvailable: false,
+      store: _UnsupportedStore(),
+    );
+    expect(find.text('Google 백업'), findsNothing);
+    expect(find.text('iCloud 백업'), findsNothing);
+    expect(find.text('개인정보 처리방침'), findsOneWidget);
   });
 }
